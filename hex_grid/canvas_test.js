bfa9b1c9c0c7a68fdf667f73c5f98a8e26277688
Falaventho <falaventho@gmail.com>
2026-02-04 15:55:39 -0500

    added simple hex grid example

---
+++ hex_grid/canvas_test.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById("mainCanvas");
+const ctx = canvas.getContext("2d");
+
+const a = (2 * Math.PI) / 6;
+const r = 50;
+
+function drawHexagon(x, y) {
+  ctx.beginPath();
+  for (var i = 0; i < 6; i++) {
+    ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
+
+function drawGrid(width, height) {
+  for (let y = r; y + r * Math.sin(a) < height; y += r * Math.sin(a)) {
+    for (
+      let x = r, j = 0;
+      x + r * (1 + Math.cos(a)) < width;
+      x += r * (1 + Math.cos(a)), y += (-1) ** j++ * r * Math.sin(a)
+    ) {
+      drawHexagon(x, y);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  drawGrid(canvas.width, canvas.height);
+});
